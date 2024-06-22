5d10f7820f514fe60e58abf6322f7cf264be3526
Vishwas Sengar <122535452+Vishwas7803@users.noreply.github.com>
2024-06-22 10:56:27 +0530

    Add files via upload

---
+++ constobject.js
@@ -0,0 +1,19 @@
+const product={
+    name :"pen",
+    typre:"ball",
+    price:"120",
+}
+//console.log(product)
+const profile={
+    name:"xyz",
+    age:10,
+    posts:20,
+    follower:30,
+    following:15
+}
+console.log(profile)
+console.log(typeof profile["name"])
+console.log(typeof profile["age"])
+console.log(typeof profile["posts"])
+console.log(typeof profile["follower"])
+console.log(typeof profile["following"])
